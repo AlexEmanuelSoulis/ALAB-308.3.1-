8737e6f327961699736eb008e4c75d6055132c75
AlexEmanuelSoulis <ebalexsoulis@outlook.com>
2025-08-06 18:23:16 -0400

    refacor again, done

--- script_lab.js
+++ script_lab.js
@@ -1,25 +1,5 @@
 let csvStr = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+let lines = csvStr.split("\n");
+let result = [];
 
-let line = csvStr.split(`\n`);
-let header = line[0].split(",");
-console.log(line);
 
-let firstLine = line[0];
-console.log(firstLine);
-let commaResult;
-let lines;
-let result =  []
-
-for(let i = 1; i < line.length; i++){
-  let rowObj = {};
-  lines = line[i].split(",");
-  for(let j = 0; j < header.length; j++){
-    rowObj[header[j]] = lines[j]
-  }
-  console.log(lines);
-
-  result.push(rowObj);
-
-}
-
-console.log("Nomber total people :")
